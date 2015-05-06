9ee66e4879c147875e8ccbb802ed52d9c4756af8
andela-kochu <kingsley.ochu@andela.co>
2015-05-06 17:20:09 +0100

    updated user schema to cover methods

--- spec/models.spec/users.spec.js
+++ spec/models.spec/users.spec.js
@@ -29,7 +29,7 @@ describe('UserSchema', function() {
         expect(userSchemaObject['name'].options.required).toBe('Name must be present');
         expect(userSchemaObject['emailAddress'].options.required).toBe('Email address can not be empty');
     });
-    /*  it('should create a new user', function() {
+    it('should create a new user', function() {
         User.create(newuser, function(err, info){
             if(err){
                 expect(err).toBeDefined();
@@ -37,10 +37,42 @@ describe('UserSchema', function() {
         expect(info.name).toBe('Kingsley');
         expect(info.emailAddress).toBe('chitech247@gmial.com');
         });
-    });*/
+    });
      it('should throw an error when data is not sufficient, name or emailAddress', function() {
         User.create({name: 'Kingsley'}, function(err, info){
         expect(err).toBeDefined();
+        });
+    });
+   it('should find users', function() {
+        User.find(function(err, info){
+            if(err){
+                expect(err).toBeDefined();
+            }
+        var type = typeof info;
+        expect(type).toEqual('object');
+        expect(info.length).toBeGreaterThan(99);
+        });
+    });
+    it('should update users', function() {
+        User.find(function(err, info){
+         var id = info[1]._id;
+            User.update({_id: id}, {name: "george"}, function(err, info2){
+                User.find({_id: id}, function(err, info3){
+                    expect(info3[0].name).toEqual('george');
+                });
+             });
+        });
+    });
+    it('should delete the first user', function() {
+        User.find(function(err, info){
+         var id = info[1]._id;
+            User.remove({_id: id}, function(err, info2){
+                User.find(function(err, info3){
+                    info3.forEach(function(elem){
+                        expect(elem._id).not.toBe(id);
+                    });
+                });
+             });
         });
     });
 });
