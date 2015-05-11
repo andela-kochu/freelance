8a6eb2eed3bcae54a3a0670e6e251f890248e6dc
andela-kochu <kingsley.ochu@andela.co>
2015-05-11 12:42:46 +0100

    nice

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function() {
-  var db  = mongoose.connect(config.db);
+  var db  = mongoose.connect(process.env.MONGOLAB_URI);
   require('../models/users.models');
   return db;
 };
