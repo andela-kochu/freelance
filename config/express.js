1401e700a5f783f6a8193fb53481e7a123e8f755
andela-kochu <kingsley.ochu@andela.co>
2015-05-16 11:19:06 +0100

    allow access origin

--- config/express.js
+++ config/express.js
@@ -23,7 +23,7 @@ module.exports = function() {
   app.use(passport.initialize());
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-   // res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     next();
   });
   require('./passport')();
