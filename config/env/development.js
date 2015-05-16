82018609b958b6eef66429d04d155711a023ab87
andela-kochu <kingsley.ochu@andela.co>
2015-05-16 01:50:41 +0100

    testing deployment

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/freelanceDb',
+  db: 'mongodb://localhost/freelanceDatabase',
   google: {
     clientID: '299601960993-eiiersos5qmc4bv4jn7sfkgsd8qk7d4m.apps.googleusercontent.com',
     clientSecret: 'IOmXjxcxj0UxwOtBef0x1Yz9'
