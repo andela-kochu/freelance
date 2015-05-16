f5c3b02ed4fc62da0bc596219e4c2e4111de28b6
andela-kochu <kingsley.ochu@andela.co>
2015-05-16 09:23:01 +0100

    debugging

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 module.exports = function(app, passport) {
   require('./auth.route')(app, passport);
   require('./comments.route')(app);
- // require('./jobs.route')(app);
+  require('./jobs.route')(app);
   require('./tags.route')(app);
   require('./users.route')(app);
 };
