37022060d46ffa97cad3bdc3154fb02eec78ad4c
andela-kochu <kingsley.ochu@andela.co>
2015-05-17 18:06:10 +0100

    added put and delete to Access-Control-Allow-Methods

--- config/express.js
+++ config/express.js
@@ -24,6 +24,7 @@ module.exports = function() {
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    res.header('Access-Control-Allow-Methods', "POST, PUT, DELETE, GET");
     next();
   });
   require('./passport')();
