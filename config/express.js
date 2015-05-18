ff2b402d7ca428adc81d9d0370a7a532620447d0
andela-kochu <kingsley.ochu@andela.co>
2015-05-18 05:41:32 +0100

    added the admin access header allow

--- config/express.js
+++ config/express.js
@@ -23,7 +23,7 @@ module.exports = function() {
   app.use(passport.initialize());
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    res.header('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, x_access_admin, Authorization, X-Requested-With");
     res.header('Access-Control-Allow-Methods', "POST, PUT, DELETE, GET");
     next();
   });
